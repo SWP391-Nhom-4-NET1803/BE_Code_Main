359cd503f53d7d3bacfa110605b54c9e6c268576
Collin Phan <collincomms+github@gmail.com>
2024-06-16 01:03:12 +0700

    Added ClinicController
    + Forgot to stage IClinicService to change list.
    (Some modification were made)
    
    + Added ClinicController (all authorization removed for testing purpose)

---
+++ Services/ClinicsService/IClinicsService.cs
@@ -0,0 +1,27 @@
+﻿using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RepoClinicService = Repositories.Models.ClinicService;
+
+namespace Services.ClinicsService
+{
+    public interface IClinicsService: IDisposable
+    {
+        bool CreateClinicService(ClinicService service);
+
+        bool CreateClinic(ClinicRegistrationModel clinc, out string message);
+
+        Clinic? GetClinicInformation(int clinicId);
+
+        public ClinicService GetServiceInfo(int serviceId);
+
+        IEnumerable<RepoClinicService> GetClinicServices(int clinicId);
+
+        bool UpdateClinicInformation(Clinic clinic, out string message);
+
+    }
+}
--- WebAPI/Controllers/ClinicsController.cs
+++ WebAPI/Controllers/ClinicsController.cs
@@ -1,9 +1,20 @@
-﻿using Core.HttpModels;
+﻿using AutoMapper;
+using Azure;
+using Core.HttpModels;
+using Core.HttpModels.ObjectModels.BookingModels;
+using Core.HttpModels.ObjectModels.ClinicModels;
+using Core.HttpModels.ObjectModels.RegistrationModels;
+using Core.HttpModels.ObjectModels.RoleModels;
+using Core.HttpModels.ObjectModels.UserModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
 using Repositories.Models;
+using Services.BookingService;
+using Services.ClinicsService;
+using Services.EmailSerivce;
+using Services.UserService;
 using System.Net;
 using System.Security.Claims;
 using WebAPI.Helper.AuthorizationPolicy;
@@ -12,27 +23,46 @@ namespace WebAPI.Controllers
 {
     [Route("api/clinic")]
     [ApiController]
-    [AllowAnonymous]
     public class ClinicsController : ControllerBase
     {
-        private readonly DentalClinicPlatformContext _DbContext;
-        private readonly IConfiguration _configuration;
         private readonly UnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public ClinicsController(IConfiguration config, DentalClinicPlatformContext context)
+        public ClinicsController(IMapper mapper, DentalClinicPlatformContext context)
         {
-            _configuration = config;
-            _DbContext = context;
-            _unitOfWork = new UnitOfWork(_DbContext);
+            _unitOfWork = new UnitOfWork(context);
+            _mapper = mapper;
         }
 
         [HttpPost]
-        [Route("register")]
-        [JwtTokenAuthorization]
+        [Route("register-clinic")]
+        //[JwtTokenAuthorization(RoleModel.Roles.ClinicStaff)]
         public async Task<IActionResult> RegisterClinic([FromBody] ClinicRegistrationModel requestObject)
         {
+            var clinicsService = HttpContext.RequestServices.GetService<IClinicsService>()!;
 
-            // Validate user authorization information
+            if (clinicsService.CreateClinic(requestObject, out var message))
+            {
+                return Ok(new HttpResponseModel()
+                {
+                    StatusCode = 200,
+                    Message = "Request accepted",
+                    Detail = message
+                });
+            }
+            else
+            {
+                return BadRequest(new HttpResponseModel()
+                {
+                    StatusCode = 400,
+                    Message = "Unable to process request",
+                    Detail = message
+                });
+            }
+
+            // ! DO NOT REMOVE THE FOLLOWING PIECE OF CODE!
+
+            /*// Validate user authorization information
             var user = (User?) HttpContext.Items["user"];
 
             if (user == null)
@@ -40,7 +70,7 @@ namespace WebAPI.Controllers
                 return Ok(new HttpResponseModel() { StatusCode = 401, Message = "You are unauthorized"});
             }
 
-            //Get staff info
+            //Get owner info info
             ClinicStaff staff = _unitOfWork.UserRepository.GetStaffInfo(user.UserId)!;
 
             // Check if the user is a clinic owner or not, whether the owner already created a clinic before
@@ -64,7 +94,7 @@ namespace WebAPI.Controllers
                 });
             }
 
-            if (!_unitOfWork.ClinicRepository.CheckClinicAvailability(requestObject.Name, out var responseMessage))
+            if (!_unitOfWork.ClinicRepository.CheckClinicAvailability(requestObject.Name!, out var responseMessage))
             {
                 return Ok(new HttpResponseModel()
                 {
@@ -77,20 +107,20 @@ namespace WebAPI.Controllers
             {
                 Clinic newClinic = new Clinic()
                 {
-                    Name = requestObject.Name,
-                    Address = requestObject.Address,
-                    Phone = requestObject.Phone,
-                    Email = requestObject.Email,
-                    OpenHour = TimeOnly.Parse(requestObject.OpenHour),
-                    CloseHour = TimeOnly.Parse(requestObject.CloseHour),
+                    Name = requestObject.Name!,
+                    Address = requestObject.Address!,
+                    Phone = requestObject.Phone!,
+                    Email = requestObject.Email!,
+                    OpenHour = TimeOnly.Parse(requestObject.OpenHour!),
+                    CloseHour = TimeOnly.Parse(requestObject.CloseHour!),
                     Status = true,
                     Owner = _unitOfWork.UserRepository.GetById(staff.UserId)!
                 };
 
                 // Add clinic services
-                foreach (var serviceId in requestObject.ClinicServices)
+                foreach (var serviceId in requestObject.ClinicServices!)
                 {
-                    var service = await _DbContext.Services.FindAsync(serviceId);
+                    var service = await _unitOfWork._context.Services.FindAsync(serviceId);
                     if (service != null)
                     {
                         newClinic.ClinicServices.Add(new ClinicService
@@ -110,37 +140,251 @@ namespace WebAPI.Controllers
                         });
                     }
                 }
+                
+                // Add the clinic to the owner inside ClinicStaff table.
                 staff.Clinic = newClinic;
                 _unitOfWork.clinicStaffRepository.Update(staff);
                 _unitOfWork.ClinicRepository.Add(newClinic);
                 _unitOfWork.Save();
 
-                return Ok(new HttpResponseModel()
+                var clinicsService = HttpContext.RequestServices.GetService<IClinicsService>()!;
+
+                if (clinicsService.CreateClinic(requestObject, out var message))
                 {
-                    StatusCode = 202,
-                    Message = "Request accepted",
-                    Detail = $"Created clinic {newClinic.Name}."
-                });
-            }
-            catch (DbUpdateException dbEx)
-            {
-                var innerExceptionMessage = dbEx.InnerException?.Message ?? dbEx.Message;
-                return Ok(new HttpResponseModel()
+                    return Ok(new HttpResponseModel()
+                    {
+                        StatusCode = 200,
+                        Message = "Request accepted",
+                        Detail = message
+                    });
+                }
+                else 
                 {
-                    StatusCode = 500,
-                    Message = "Internal Server Error",
-                    Detail = innerExceptionMessage
-                });
+                    return BadRequest(new HttpResponseModel()
+                    {
+                        StatusCode = 400,
+                        Message = "Unable to process request",
+                        Detail = message
+                    });
+                }
+                
             }
             catch (Exception e)
             {
-                return Ok(new HttpResponseModel()
+                return BadRequest(new HttpResponseModel()
                 {
                     StatusCode = 500,
                     Message = "Internal Server Error",
-                    Detail = e.Message
+                    Detail = e.Message,
+                    Content = e.InnerException?.Message
                 });
+            }*/
+        }
+
+        [HttpPost]
+        [Route("register-owner")]
+        //[AllowAnonymous]
+        public ActionResult<IHttpResponseModel<object>> RegisterClinicOwner([FromBody] UserRegistrationModel requestObject)
+        {
+            var userService = HttpContext.RequestServices.GetService<IUserService>()!;
+
+            if (!userService.CreateClinicStaff(requestObject, true, out var message))
+            {
+                return BadRequest(new HttpResponseModel() { StatusCode = 202, Message = "Can not create owner account", Detail = message });
             }
+
+            _unitOfWork.Save();
+
+            //
+            /*var emailService = HttpContext.RequestServices.GetService<IEmailService>()!;
+
+            string emailSubject = "Xác nhận yêu cầu tạo tài khoản người dùng";
+
+            string emailBody =
+                $"<p>Xin chào người dùng {requestObject.Username}! </p>" +
+                $"Chúng tôi đã nhận được yêu cầu tạo tài khoản cho email {requestObject.Email}, cảm ơn bạn đã đăng kí dịch vụ của chúng tôi. </p>" +
+                $"<p>Vui lòng xác thực tài khoản thông qua cổng xác thực của chúng tôi tại <a href=\"http://localhost:5173/user/auth\"></a></p>";
+
+            await emailService.SendMailGoogleSmtp(requestObject.Email!, subject: emailSubject, body: emailBody);*/
+
+            return Ok(new HttpResponseModel() { StatusCode = 202, Message = "Yêu cầu tạo mới người dùng đang được xử lí." });
+        }
+
+        [HttpPost]
+        [Route("register-dentist")]
+        //[JwtTokenAuthorization(RoleModel.Roles.ClinicStaff)]
+        public ActionResult<IHttpResponseModel<object>> RegisterDentist([FromBody] UserRegistrationModel requestObject)
+        {
+
+            User invoker = (User)HttpContext.Items["user"]!;
+
+            var userService = HttpContext.RequestServices.GetService<UserService>()!;
+
+            ClinicStaff owner = userService.GetClinicStaffInfoById(invoker.UserId)!; // Getting the clinic owner information (just get the clinic ID)
+
+            if (!owner.IsOwner)
+            {
+                return BadRequest(new HttpResponseModel() { StatusCode = 401, Message = "Unauthorized", Detail = "You don't have access for this command." });
+            }
+
+            requestObject.Clinic = owner.ClinicId; // Append the clinic Id from the owner to the new clinic staff.
+
+            if (!userService.CreateClinicStaff(requestObject, false, out var message))
+            {
+                return BadRequest(new HttpResponseModel() { StatusCode = 400, Message = "Failed", Detail = message });
+            }
+
+            _unitOfWork.Save();
+
+            /*var emailService = HttpContext.RequestServices.GetService<IEmailService>()!;
+
+            string emailSubject = "Xác nhận yêu cầu tạo tài khoản người dùng";
+
+            string emailBody =
+                $"<p>Xin chào người dùng {requestObject.Username}! </p>" +
+                $"Chúng tôi đã nhận được yêu cầu tạo tài khoản cho email {requestObject.Email} bởi chủ phòng khám. Hãy kiểm tra lại thông tin nếu bạn nhận được thông báo này. </p>" +
+                $"<p>Vui lòng xác thực tài khoản thông qua cổng xác thực của chúng tôi tại <a href=\"http://localhost:5173/user/auth\"></a></p>";
+
+            await emailService.SendMailGoogleSmtp(requestObject.Email!, subject: emailSubject, body: emailBody);
+            await emailService.SendMailGoogleSmtp(invoker.Email, subject: emailSubject, body: emailBody);*/
+
+            return Ok(new HttpResponseModel() { StatusCode = 200, Message = "User account creation is succeed" });
+        }
+
+        [HttpGet("clinic-book")]
+        //[JwtTokenAuthorization(RoleModel.Roles.ClinicStaff)]
+        public ActionResult<IHttpResponseModel<List<BookingModel>>> GetClinicBooking([FromQuery] int clinicId)
+        {
+            var clinicService = HttpContext.RequestServices.GetService<IClinicsService>()!;
+            var bookingService = HttpContext.RequestServices.GetService<IBookingService>()!;
+
+            if (clinicService.GetClinicInformation(clinicId) == null)
+            {
+                return BadRequest(new HttpResponseModel() { StatusCode = 404, Message = "Failed", Detail = "No clinic found with provided id!" });
+            }
+
+            var appointments = bookingService.getClinicBooking(clinicId, true);
+
+            return Ok(new HttpResponseModel() { StatusCode = 200, Message = "Succed", Content = appointments });
+        }
+
+        [HttpGet("{id}/staffs")]
+        //[JwtTokenAuthorization(RoleModel.Roles.ClinicStaff)]
+        public ActionResult<IHttpResponseModel<List<ClinicStaffInfoModel>>> GetAllStaffInfo([FromQuery] int id)
+        {
+            var user = (User)HttpContext.Items["user"]!;
+
+            var userService = HttpContext.RequestServices.GetService<IUserService>()!;
+
+            var staff = userService.GetAllClinicStaffInfo(id);
+
+            if (staff == null)
+            {
+                return NotFound(new HttpResponseModel() { StatusCode = 404, Message = "User info not found" });
+            }
+
+            var staffInfo = _mapper.Map<IEnumerable<ClinicStaffInfoModel>>(staff);
+
+
+            return Ok(new HttpResponseModel() { StatusCode = 200, Message = "Succeed",Content = staffInfo });
+        }
+
+        [HttpGet("{id}/staff")]
+        //[JwtTokenAuthorization(RoleModel.Roles.ClinicStaff)]
+        public ActionResult<IHttpResponseModel<ClinicStaffInfoModel>> GetStaffInfo([FromQuery] int staffId)
+        {
+            var userService = HttpContext.RequestServices.GetService<IUserService>()!;
+
+            // validating invoker
+            var invoker = (User)HttpContext.Items["user"]!;
+
+            if (!userService.IsClinicOwner(invoker))
+            {
+
+            }
+
+
+            
+
+            var staff = userService.GetFromStaffId(staffId);
+
+            if (staff == null)
+            {
+                return NotFound(new HttpResponseModel() { StatusCode = 404, Message = "User info not found" });
+            }
+
+            var staffInfo = _mapper.Map<ClinicStaff, ClinicStaffInfoModel>(staff);
+
+            HttpResponseModel response = new HttpResponseModel()
+            {
+                StatusCode = 200,
+                Message = "Succeed",
+                Content = staffInfo
+            };
+
+            return Ok(response);
+        }
+
+        // =================================== Simple REST API =====================================
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public ActionResult<IHttpResponseModel<ClinicInformationModel>> Get(int id)
+        {
+            var clinicsService = HttpContext.RequestServices.GetService<IClinicsService>()!;
+
+            var clinicInfo = clinicsService.GetClinicInformation(id);
+
+            if (clinicInfo == null)
+            {
+                return NotFound(new HttpResponseModel() { StatusCode = 404, Message = "Clinic not found", Detail = "No clinic found with provided ID" });
+            }
+
+            var response = new HttpResponseModel() { StatusCode = 200, Message = "Clinic found", Content = _mapper.Map<Clinic, ClinicInformationModel>(clinicInfo) };
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public ActionResult<IHttpResponseModel<ClinicInformationModel>> Post(int id)
+        {
+            var clinicsService = HttpContext.RequestServices.GetService<IClinicsService>()!;
+
+            var clinicInfo = clinicsService.GetClinicInformation(id);
+
+            if (clinicInfo == null)
+            {
+                return NotFound(new HttpResponseModel() { StatusCode = 404, Message = "Clinic not found", Detail = "No clinic found with provided ID" });
+            }
+
+            var response = new HttpResponseModel() { StatusCode = 200, Message = "Clinic found", Content = _mapper.Map<Clinic, ClinicInformationModel>(clinicInfo) };
+
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<IHttpResponseModel<ClinicInformationModel>> Put(int id)
+        {
+            var clinicsService = HttpContext.RequestServices.GetService<IClinicsService>()!;
+
+            var clinicInfo = clinicsService.GetClinicInformation(id);
+
+            if (clinicInfo == null)
+            {
+                return NotFound(new HttpResponseModel() { StatusCode = 404, Message = "Clinic not found", Detail = "No clinic found with provided ID" });
+            }
+
+            var response = new HttpResponseModel() { StatusCode = 200, Message = "Clinic found", Content = _mapper.Map<Clinic, ClinicInformationModel>(clinicInfo) };
+
+            return Ok(response);
+        }
+
+        
+
+        [HttpDelete("{id}")]
+        public ActionResult<IHttpResponseModel<object>> DeleteClinic(int id)
+        {
+            return Ok(new HttpResponseModel() { Detail="This method is not implemented" });
         }
     }
 }
